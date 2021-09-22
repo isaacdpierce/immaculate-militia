3dab1eba6d21d65483668bb49537b3217b8c305d
The Rook <hello@isaaacpierce.io>
2021-09-22 17:20:58 +0100

    Changes text content

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,10 +97,10 @@ const IndexPage = () => {
             <br />
           </h2>
           <p style={paragraphStyles}>
-          1) To guard the spiritual, physical and mental and health of our children. 
+          1) To guard the spiritual, physical and mental and health of our children in these diabolical times.
           <br/>
           <br/>
-          2) For the wisdom, strength and perseverance of the all parents. That they continue to resist the social pressures of injecting themselves and their children with experimental, abortion-tainted vaccines. 
+          2) For the wisdom, strength and perseverance of all the parents who continue to resist the social pressures of injecting themselves and their children with experimental, abortion-tainted vaccines.
           </p>
           <h2 style={headingStyles}>
             Prayers
