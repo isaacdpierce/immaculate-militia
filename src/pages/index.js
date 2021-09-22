4830620ede8eee992835c0d75d7079d099d3e79a
The Rook <hello@isaaacpierce.io>
2021-09-22 19:57:58 +0100

    Changes text content

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,13 +81,10 @@ const IndexPage = () => {
             <br />
             <span style={headingAccentStyles}>To Our Lady of The Rosary</span>
             <br />
-            <span style={headingSubtitleStyles}>Sept. 28 - Oct. 7 (Feast Day)</span>
+            <span style={headingSubtitleStyles}>Sept. 28<sup>th</sup> - Oct. 6<sup>th</sup> </span>
           </h1>
           <p style={paragraphStyles}>
-            Join us in daily prayer to our Blessed Mother for her immediate intercession to save our children and strengthen the parents who are being bullied and manipulated into injecting an immoral, experimental “vaccine”. 
-          </p>
-          <p style={paragraphStyles}>
-            Join us in daily prayer to our Blessed Mother for her immediate intercession to save our children and strengthen the parents who are being bullied and manipulated into injecting an immoral, experimental “vaccine. 
+            Join us in daily prayer to our Blessed Mother for her immediate intercession to save our children and strengthen the resolve of parents who are being bullied and manipulated into injecting an immoral, experimental “vaccine”. 
           </p>
           <p style={paragraphStyles}>
             Virgin Most Powerful — Pray for us.
@@ -101,6 +98,9 @@ const IndexPage = () => {
           <br/>
           <br/>
           2) For the wisdom, strength and perseverance of all the parents who continue to resist the social pressures of injecting themselves and their children with experimental, abortion-tainted vaccines.
+          <br/>
+          <br/>
+          3)To open hearts, minds and eyes to the truth of this pandemic and these experimental vaccines.
           </p>
           <h2 style={headingStyles}>
             Prayers
@@ -120,6 +120,28 @@ const IndexPage = () => {
           <br />
           God grant us the strength to resist the social pressures and medical tyranny rampant in our modern world.
           </p>
+
+          <h3 style={headingStyles}>Daily Prayer</h3>
+
+          <p style={paragraphStyles}>My dearest Mother Mary, behold me, your child, in prayer at your feet. Accept this Holy Rosary, which I offer you in accordance with your requests at Fatima, as a proof of my tender love for you, for the intentions of the Sacred Heart of Jesus, in atonement for the offenses committed against your Immaculate Heart, and for this special favor which I earnestly request in my Rosary Novena:
+          </p>
+          <p style={paragraphStyles}>Please open hearts, minds and eyes to the truth of this pandemic and these experimental vaccines, guard the spiritual, physical and mental health of our children, and provide parents and caregivers with the wisdom, strength and perseverance so they have the resolve to resist the social pressures associated with these experimental vaccines. We ask that our trusted institutions cease all efforts to coerce and manipulate parents and young people into participating in this medical intervention.</p>
+          <p style={paragraphStyles}>
+            I beg you to present my petition to your Divine Son. If you will pray for me, I cannot be refused. I know, dearest Mother, that you want me to seek God’s holy Will concerning my request. If what I ask for should not be granted, pray that I may receive that which will be of greater benefit to my soul.
+            I offer you this spiritual “Bouquet of Roses” because I love you. I put all my confidence in you, since your prayers before God are most powerful. For the greater glory of God and for the sake of Jesus, your loving Son, hear and grant my prayer. Sweet Heart of Mary, be my salvation. 
+          </p>
+          <p style={paragraphStyles}>Our Father, Hail Mary & Glory Be...</p>
+
+
+          <h2 style={headingStyles}>Optional Prayer
+          <br />
+          <br />
+          <span style={headingSubtitleStyles}>For the Prayer Warriors</span>
+          </h2>
+
+
+{/* ======================================================== */}
+
           <h3 style={headingStyles}>Padre Pio’s Secret Weapon Prayer
           <br />
           <br />
@@ -139,7 +161,10 @@ I. O my Jesus, you have said: “Truly I say to you, ask and you will receive, s
           <br/>
           2) The wisdom, strength and perseverance of the all parents. That they continue to resist the social pressures of injecting themselves and their children with experimental, abortion-tainted vaccines. 
           <br/>
-<br/>
+          <br/>
+          3)To open hearts, minds and eyes to the truth of this pandemic and these experimental vaccines.
+          <br/>
+          <br/>
 Our Father….Hail Mary….Glory Be to the Father….Sacred Heart of Jesus, I place all my trust in you.
   </p>
 
@@ -152,9 +177,12 @@ II. O my Jesus, you have said: “Truly I say to you, if you ask anything of the
           <br/>
           2) The wisdom, strength and perseverance of the all parents. That they continue to resist the social pressures of injecting themselves and their children with experimental, abortion-tainted vaccines. 
           <br/>
-<br/>
-Our Father….Hail Mary….Glory Be to the Father….Sacred Heart of Jesus, I place all my trust in you.
-  </p>
+          <br/>
+          3)To open hearts, minds and eyes to the truth of this pandemic and these experimental vaccines.
+          <br/>
+          <br/>
+          Our Father….Hail Mary….Glory Be to the Father….Sacred Heart of Jesus, I place all my trust in you.
+</p>
 
 <p style={paragraphStyles}>
 III. O my Jesus, you have said: “Truly I say to you, heaven and earth will pass away but my words will not pass away.” Encouraged by your infallible words I now ask for:
@@ -165,6 +193,9 @@ III. O my Jesus, you have said: “Truly I say to you, heaven and earth will pas
           <br/>
           2) The wisdom, strength and perseverance of the all parents. That they continue to resist the social pressures of injecting themselves and their children with experimental, abortion-tainted vaccines. 
           <br/>
+          <br/>
+          3)To open hearts, minds and eyes to the truth of this pandemic and these experimental vaccines.
+          <br/> 
 <br/>
 Our Father….Hail Mary….Glory Be to the Father….Sacred Heart of Jesus, I place all my trust in you.
   </p>
